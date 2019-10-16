9ba73df19d374b963e040d348d9b20cabf1b1d38
Marco Santos <marco.santos@glartek.com>
2019-10-16 17:51:18 +0100

    Add job plan

--- nomad/operations.go
+++ nomad/operations.go
@@ -22,3 +22,11 @@ func (d *Driver) RegisterJob(job *api.Job) (*api.JobRegisterResponse, error) {
 	res, _, err := d.client.Jobs().Register(job, nil)
 	return res, err
 }
+
+// PlanJob plans a job on the nomad cluster
+func (d *Driver) PlanJob(job *api.Job) (*api.JobPlanResponse, error) {
+
+	// Register the task
+	res, _, err := d.client.Jobs().Plan(job, true, nil)
+	return res, err
+}

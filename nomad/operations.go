05f567bbdbe79ebe93aa1ea13488921dbf314cef
Urjit Singh Bhatia <urjitsb87@gmail.com>
2021-01-18 15:41:31 -0800

    add ability to wait for deployments

--- nomad/operations.go
+++ nomad/operations.go
@@ -1,7 +1,9 @@
 package nomad
 
 import (
+	"fmt"
 	"github.com/hashicorp/nomad/api"
+	"time"
 )
 
 // ParseTemplate ...
@@ -32,4 +34,33 @@ func (d *Driver) PlanJob(job *api.Job) (*api.JobPlanResponse, error) {
 	// Register the task
 	res, _, err := d.client.Jobs().Plan(job, true, nil)
 	return res, err
+}
+
+func (d *Driver) WatchDeployment(job *api.JobRegisterResponse, timeout time.Duration) error {
+	// get the deployment id for this alloc
+	eval, _, err := d.client.Evaluations().Info(job.EvalID, nil)
+	if err != nil {
+		return err
+	}
+
+	timer := time.NewTimer(timeout)
+	for {
+		select {
+		case <-timer.C:
+			return fmt.Errorf("deployment watcher timedout. Deployment: %s", eval.DeploymentID)
+		default:
+			deployment, _, err := d.client.Deployments().Info(eval.DeploymentID, nil)
+			if err != nil {
+				return err
+			}
+			switch deployment.Status {
+			case "successful":
+				return nil
+			case "running":
+				time.Sleep(time.Second * 5)
+			default:
+				return fmt.Errorf("deployment: %s failed. Status: %s : %s", eval.DeploymentID, deployment.Status, deployment.StatusDescription)
+			}
+		}
+	}
 }
